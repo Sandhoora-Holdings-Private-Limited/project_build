b58d467e0b22dc08adc3d44fb9b80cc4dde0feae
Amila95 <amilawicramarathna95@gmail.com>
2017-12-02 06:13:40 +0530

    set customer controller

--- application/controllers/Customer.php
+++ application/controllers/Customer.php
@@ -11,9 +11,35 @@ class Customer extends CI_Controller
 
     public function index()
     {
-        $data['page'] = array('header'=>'', 'description'=>'');
-        $data['user'] = array('name'=>'', 'email'=>'');
-        $this->load->view('template/header',$data);
-        $this->load->view('template/footer');
+        if(isset($_SESSION['user']))
+        {
+            $data['page'] = array('header'=>'Customer', 'description'=>'pick a customer or create new one','app_name'=>'CUSTOMER');
+            $data['user'] = $_SESSION['user'];
+            $data['apps'] = $_SESSION['apps'];
+            $this->load->view('template/header',$data);
+            $this->load->view('Customer/index',$data);
+            $this->load->view('template/footer');
+        }
+        else
+        {
+            redirect('/Main/login', 'refresh');
+        }
+    }
+    public function new()
+    {
+        if(isset($_SESSION['user']))
+        {
+            $data['page'] = array('header'=>'Customer New', 'description'=>'create a new customer','app_name'=>'CUSTOMER');
+            $data['user'] = $_SESSION['user'];
+            $data['apps'] = $_SESSION['apps'];
+            $this->load->view('template/header',$data);
+            $this->load->view('Customer/new',$data);
+            $this->load->view('template/footer');
+        }
+        else
+        {
+            redirect('/Main/login', 'refresh');
+        }
+
     }
 }
